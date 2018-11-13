d2f333b918681e621b16058f699b6790fbcbfb39
John-df <44902610+John-df@users.noreply.github.com>
2018-11-13 15:35:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
     DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
-    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
     
     PG_CONFIG: {  
         user: process.env.PG_CONFIG_USER,  
