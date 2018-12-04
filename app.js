7b0b57992ba300800fe975e9356ed86dd7683aa9
John-df <44902610+John-df@users.noreply.github.com>
2018-12-04 13:01:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -283,7 +283,7 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
 				let taille=  contexts.lenght;
 				console.log('--------> noms des contexts (taille :'+taille+'): ');
 				var j=0;
-				for(var i=0;i<taille;i++){
+				for(var i=0;i<taille;++i){
 					//console.log(JSON.stringify(contexts[i].name));
 					if(contexts[i].name.includes('espace_travail_request-followup'){
 					   j=i;
