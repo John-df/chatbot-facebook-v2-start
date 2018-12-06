190d7e27f713651cca0ed55e8b4549bdcff86811
John-df <44902610+John-df@users.noreply.github.com>
2018-12-06 12:50:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -342,7 +342,9 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
 					
 					else {
 						handleMessages(messages, sender);
+						console.log('---else in while loop yeah');
 						if(replies.length!=0){
+							console.log("obj false activation agent 47 quickreplies");
 							sendQuickReply(sender,title,replies);
 						}
 					}
