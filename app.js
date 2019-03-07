99de4c42219ecd381822daaa86c6d9321a518ab3
John-df <44902610+John-df@users.noreply.github.com>
2019-03-07 12:21:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
 
 'use strict';
 
-const dialogflow = require('dialogflow');
-const config = require('./config');
 const express = require('express');
-const crypto = require('crypto');
-const bodyParser = require('body-parser');
-const request = require('request');
 const app = express();
-const uuid = require('uuid');
-
-let langue_bot="";
 
 app.set('port', (process.env.PORT || 5000))
-
-//verify request came from facebook
-app.use(bodyParser.json({
-	verify: verifyRequestSignature
-}));
-
-//serve static files in the public directory
-app.use(express.static('public'));
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-
-// Process application/json
-app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
