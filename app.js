70e863de21a13db7b88a54788fc852cd68911f70
John-df <44902610+John-df@users.noreply.github.com>
2019-03-07 12:41:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 
 'use strict';
 
+const dialogflow = require('dialogflow');
+const config = require('./config');
 const express = require('express');
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+const uuid = require('uuid');
 
 app.set('port', (process.env.PORT || 5000))
+
+//verify request came from facebook
+app.use(bodyParser.json({
+	verify: verifyRequestSignature
+}));
+
+//serve static files in the public directory
+app.use(express.static('public'));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Process application/json
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
+})
+
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	console.log("request");
+	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
 })
 
 // Spin up the server
